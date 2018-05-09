96ad2bc221b3d194873f362a84919d194620d642
zemarchezi <jose.marchezi@inpe.br>
2018-05-09 19:24:23 -0300

    init

--- testeDonw.py
+++ testeDonw.py
@@ -2,7 +2,8 @@ from ReadGroundBasedData import ReadCarisma
 import datetime
 import matplotlib.pyplot as plt
 
-rd = ReadCarisma(inidate=datetime.datetime(2014,03,25,00,00), enddate=datetime.datetime(2014,03,25,23,59))
+# rd = ReadCarisma(inidate=datetime.datetime(2014,03,25,00,00), enddate=datetime.datetime(2014,03,25,23,59))
+rd = ReadCarisma(inidate=datetime.datetime(2013,05,25,00,00), enddate=datetime.datetime(2013,05,25,23,59))
 
 rd.directories(downlDir='/home/jose/Documents/carisma_data/', plotDir='/home/jose/MEGA/Doutorado/Progs/Read_GroundMagData/plot')
 
