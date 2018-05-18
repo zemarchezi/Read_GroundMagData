cd65977e1aa37040ef2de215fb0962afc600b8d8
zemarchezi <jpmarchezi@gmail.com>
2018-05-18 16:47:04 -0300

    Read the magnetic components

--- ReadSupermag.py
+++ ReadSupermag.py
@@ -19,13 +19,14 @@ __author__ = 'zemarchezi'
 
 class ReadSupermag(object):
     """docstring for ReadSupermag."""
-    def __init__(self, inidate='', enddate='', station=''):
+    def __init__(self, inidate='', enddate='', station='', filen = ''):
         # Initial time
         self.ini = inidate
         ###
         # End time
         self.end = enddate
         self.station = station
+        self.filen = filen
 
     def directories(self, downlDir, plotDir):
         ####
@@ -41,3 +42,47 @@ class ReadSupermag(object):
         self.plotDir = plotDir
 
     def files(self):
+        self.filename = self.dataDownlDir + self.filen
+        self.files = glob.glob(self.filename)
+        self.files.sort()
+
+    def readInfo(self):
+        f = open(self.filename, 'r')
+
+        self.data = f.readlines()
+        f.close()
+
+
+        self.stations = filter(lambda x: '-mlt' in x, self.data)[0].split('-mlt')
+
+        # print(len(time))
+        year = self.stations[1][17:21]
+        Date = filter(lambda x: year+'\t' in x, self.data)
+        self.time = []
+        for d in Date:
+            self.time.append(datetime.datetime(int(d[0:4]),int(d[5:7]),int(d[8:10]),int(d[11:13]),
+                       int(d[14:16]),int(d[17:19]),int(d[20:22])))
+        #
+        #
+        # #stations = (stations[0][-(len(stations)-95):-1]).split(',')
+        self.stations = self.stations[1][49:-1].split(',')
+
+
+    def readData(self):
+        x, y, z = [], [], []
+        for i in self.stations:
+            temp_x, temp_y, temp_z = [], [], []
+            d = filter(lambda x: i+'\t' in x, self.data)
+            for k in d:
+                temp_x.append(float(((k.split('\n')[0]).split('\t'))[1]))
+                temp_y.append(float(((k.split('\n')[0]).split('\t'))[2]))
+                temp_z.append(float(((k.split('\n')[0]).split('\t'))[3]))
+            x.append(temp_x)
+            y.append(temp_y)
+            z.append(temp_z)
+
+        self.north = pd.DataFrame(data=np.transpose(x), columns=self.stations, index=self.time)
+        self.east = pd.DataFrame(data=np.transpose(y), columns=self.stations, index=self.time)
+        self.down = pd.DataFrame(data=np.transpose(z), columns=self.stations, index=self.time)
+
+        return (self.north, self.east , self.down)
