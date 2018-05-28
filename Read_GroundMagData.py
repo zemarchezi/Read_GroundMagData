8023f2d4f5e976c22ca28f2511b1ce04a1044865
zemarchezi <jpmarchezi@gmail.com>
2018-05-28 08:39:26 -0300

    read carisma components

--- Read_GroundMagData.py
+++ Read_GroundMagData.py
@@ -2,29 +2,26 @@
 # encoding: iso-8859-1
 # encoding: win-1252
 """
-Created onmain. May  8, 2018
-
+Created on May  018
 @author: zemarchezi
 """
 import numpy as np
-import matplotlib.pylab as plt
 import datetime
-import os, re, ssl, urllib, sys, fnmatch
+import os
 import glob
 import pandas  as pd
-from collections import OrderedDict
 
 __author__ = 'zemarchezi'
 
 class ReadCarisma():
     """docstring for ReadGrBasedData."""
-    def __init__(self, inidate='', enddate='', station=''):
+    def __init__(self, inidate='', enddate=''):
         # Initial time
         self.ini = inidate
         ###
         # End time
         self.end = enddate
-        self.station = station
+        # self.station = station
 
 
     def directories(self, downlDir, plotDir):
@@ -44,104 +41,105 @@ class ReadCarisma():
         ####
         ## Define o diretório e nome dos arquivos que serão abertos
         #
-        filen = '%04d-%02d-%02d*%04d-%02d-%02d*/%04d%02d%02d*%s*' %(self.ini.year, self.ini.month,
-                                                  self.ini.day,self.end.year, self.end.month,self.end.day,
-                                                  self.ini.year,self.ini.month,self.ini.day,
-                                                  self.station)
-        self.filename = self.dataDownlDir + filen
-        print(self.filename)
-        self.files = glob.glob(self.filename)
-        print(self.files)
+        self.day_arr = [self.ini + datetime.timedelta(days=x) for x in range(0, int(str(self.end-self.ini)[0])+1)]
+        self.files = []
+        for i in self.day_arr:
+            filen = '**/*%04d%02d%02d*' %(i.year, i.month, i.day)
+            self.filename = self.dataDownlDir + filen
+            print(self.filename)
+            self.files.extend(glob.glob(self.filename))
+
         self.files.sort()
 
     def readData(self):
         ######
         ## Abre os arquivos e extrai as componentes
         #
-        stations, lat = [], []
-        x, y, z, h, t, mh = [], [], [], [], [], []
+        latitude = []
+        x, y, z, h, mh, t, stations = [], [], [], [], [], [], []
         for i in self.files:
             print(i)
             f = open(i, 'r')
             ss = f.readline().split(' ') ## Open the file and read the first line
             f.close()
-            stations.append(ss[0]) # Create an list with the stations names
-            lat.append(ss[2]) # Create a list with the corresponding latitudes
             ######
             ## Open and read the x, y, and z compoents
             #
-            temp_t, temp_x, temp_y, temp_z = np.loadtxt(i, skiprows=1, usecols=(0,1,2,3), unpack=True)
-            temp_h = np.sqrt(temp_x**2 + temp_y**2 + temp_z**2) # Calculate the H component
-            temp_mh = temp_h - (np.mean(temp_h)) # Subtract the mean value of the field
-            #####
-            ## Create an list with the components for each station
-            t.append(temp_t) # time array
-            x.append(temp_x) # x component
-            y.append(temp_y) # y component
-            z.append(temp_z) # z component
-            h.append(temp_h) # H component
-            mh.append(temp_mh) # mean variation of H component
-        #####
-        ## Create an time array from the data
-        time = []
-        t_sec = []
-        for i in t[0]:
-            ## Datetime formaat
-            time.append(datetime.datetime(int(str(int(i))[0:4]), int(str(int(i))[4:6]),
-                                          int(str(int(i))[6:8]), int(str(int(i))[8:10]),
-                                          int(str(int(i))[10:12]), int(str(int(i))[12:14])))
-            ## Second dta array
-            t_sec.append(int(str(int(i))[8:10])*3600 + int(str(int(i))[10:12])*60 + int(str(int(i))[12:14]))
+            try:
+                temp_t, temp_x, temp_y, temp_z = np.loadtxt(i, skiprows=1, usecols=(0,1,2,3), unpack=True)
+                temp_h = np.sqrt(temp_x**2 + temp_y**2 + temp_z**2) # Calculate the H component
+                temp_mh = temp_h - (np.mean(temp_h)) # Subtract the mean value of the field
+                #####
+                ## Create an time array from the data
+                time = []
+                t_sec = []
+                for i in temp_t:
+                    ## Datetime formaat
+                    time.append(datetime.datetime(int(str(int(i))[0:4]), int(str(int(i))[4:6]),
+                                                  int(str(int(i))[6:8]), int(str(int(i))[8:10]),
+                                                  int(str(int(i))[10:12]), int(str(int(i))[12:14])))
+                    ## Second dta array
+                    t_sec.append(int(str(int(i))[8:10])*3600 + int(str(int(i))[10:12])*60 + int(str(int(i))[12:14]))
 
-        ######
-        ## Create an table formatation for the components and filtered data
-        #
-        tab_x = pd.DataFrame(data=np.transpose(x), columns=stations, index=time)
-        tab_y = pd.DataFrame(data=np.transpose(y), columns=stations, index=time)
-        tab_z = pd.DataFrame(data=np.transpose(z), columns=stations, index=time)
-        tab_h = pd.DataFrame(data=np.transpose(mh), columns=stations, index=time)
+                station = [ss[0]]*len(time) # Create an list with the stations names
+                lat = [ss[2]]* len(time) # Create a list with the corresponding latitudes
+
+                ## Create an list with the components for each station
+                t.extend(time) # time array
+                x.extend(temp_x) # x component
+                y.extend(temp_y) # y component
+                z.extend(temp_z) # z component
+                h.extend(temp_h) # H component
+                mh.extend(temp_mh) # mean variation of H component
+                stations.extend(station)
+                latitude.extend(lat)
+            except (Exception) as e:
+                print (e)
+
+
+        dd = pd.DataFrame(np.transpose([x,y,z,h,mh]), index=t,columns=['x', 'y', 'z', 'h', 'mean_h'])
+        dd['stat'] = stations
+        dd['lat'] = latitude
+        self.dd = dd.drop_duplicates()
+#            dat.append(dd.to_dict())
+        return (self.dd)
+
+
+    def separateStat(self):
+        stat = []
+        llat = []
+        for i in self.day_arr:
+            stat.append(set(self.dd['stat'][self.dd.index==i]))
+            llat.append(set(self.dd['lat'][self.dd.index==i]))
+
+        self.eqStat = list(set.intersection(*stat))
+        self.eqlat = list(set.intersection(*llat))
+
         ######
         ## Sort the stations according to the latitudes, from higher to lower
         #
-        stat = dict(zip(stations, lat))
-        stations.sort(key=stat.get, reverse=True)
+        stat = dict(zip(self.eqStat, self.eqlat))
+        print (stat)
+        self.eqStat.sort(key=stat.get, reverse=True)
 
-        sa = []
-        for i in stations:
-            sa.append(i+ ' ' +stat[i]+'$^{\circ}$')
+        xAllStat = pd.DataFrame()
+        yAllStat = pd.DataFrame()
+        zAllStat = pd.DataFrame()
+        hAllStat = pd.DataFrame()
+        mhAllStat = pd.DataFrame()
+        for i in self.eqStat:
+            xAllStat[i] = self.dd[self.dd['stat']==i]['x']
+            yAllStat[i] = self.dd[self.dd['stat']==i]['y']
+            zAllStat[i] = self.dd[self.dd['stat']==i]['z']
+            hAllStat[i] = self.dd[self.dd['stat']==i]['h']
+            mhAllStat[i]= self.dd[self.dd['stat']==i]['mean_h']
 
-        newname = dict(zip(stations, sa))
-
-        ######
-        ## Order the Keys of the components and filtered data DataFrames
-        temp_x = OrderedDict.fromkeys(stations)
-        temp_y = OrderedDict.fromkeys(stations)
-        temp_z = OrderedDict.fromkeys(stations)
-        temp_h = OrderedDict.fromkeys(stations)
-        ######
-        ## Order the position of the coluns in the Data Frames
-        #
-        for i in stations:
-            temp_x[i] = tab_x[i]
-            temp_y[i] = tab_y[i]
-            temp_z[i] = tab_z[i]
-            temp_h[i] = tab_h[i]
-        #####
-        ## Crete the definitive Data Frame for the data
-        #
-        x = pd.DataFrame(temp_x)
-        y = pd.DataFrame(temp_y)
-        z = pd.DataFrame(temp_z)
-        h = pd.DataFrame(temp_h)
-        x = x.rename(columns=newname)
-        y = y.rename(columns=newname)
-        z = z.rename(columns=newname)
-        h = h.rename(columns=newname)
-
-        return ([x, y, z, h])
+        return ([xAllStat,yAllStat,zAllStat,hAllStat,mhAllStat, self.eqlat])
 
 
-
+####################################################################################
+#  Supermag
+####################################################################################
 class ReadSupermag(object):
     """docstring for ReadSupermag."""
     def __init__(self, station='', filen = ''):
--- testeDonw.py
+++ testeDonw.py
@@ -1,58 +1,82 @@
-from Read_GroundMagData import ReadIntermagnet
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
+from Read_GroundMagData import ReadCarisma
 import datetime
 import matplotlib.pyplot as plt
 from DataDownloader import DataDownloader
 import os, re, ssl, urllib, sys, fnmatch
-# inidate=datetime.datetime(2016,6,24,0,0), enddate=datetime.datetime(2016,6,28,0,0),
 
-station = 'kou'
-year = '2015'
-month = '05'
-days = ['23']
-host = 'ftp.intermagnet.org' # ftp host name
-user = 'imaginpe' # ftp user name
-password = 'd@a^DGE' # ftp password
-# Directory contain the data
-working_directory = 'minute/variation/IAGA2002/'+year+'/'+month+'/'
-
-dataDownlDir = '/home/jose/MEGA/Doutorado/Progs/read_plotSpec_Intermag/dados/data20180521125025/'
-
-listFilename = []
-for d in days:
-    listFilename.append(station+year+month+d+'vmin.min')
-
-print (listFilename)
-
-# filename = station+year+month+day+'vmin.min' # data filename
-
-# Creating a instance of MarxDownloader
-mx = DataDownloader(host, user, password)
-
-mx.set_user_and_password(user, password)
-
-
-# Connecting
-mx.connectFTP()
-
-mx.set_output_directory(dataDownlDir)
-
-mx.set_directoryFTP(working_directory)
-
-mx.download_one_dataFTP(listFilename[0])
+dataDownlDir = '/home/jose/Documents/carisma_data/'
+plotDir = '/home/jose/Documents/dados/plots/'
 
 
 
+rd = ReadCarisma(inidate=datetime.datetime(2016,7,19), enddate=datetime.datetime(2016,7,20))
 
-# rd = ReadCarisma(inidate=datetime.datetime(2014,03,25,00,00), enddate=datetime.datetime(2014,03,25,23,59))
-rd = ReadIntermagnet(filen = listFilename[0])
-
-rd.directories(downlDir='/home/jose/MEGA/Doutorado/Progs/read_plotSpec_Intermag/dados/data20180521125025/', plotDir='/home/jose/MEGA/Doutorado/Progs/read_SuperMag/plots/')
+rd.directories(downlDir=dataDownlDir, plotDir=plotDir)
 
 rd.files()
 
-x = rd.readInfo()
+dat2 = rd.readData()
 
-# x = rd.readData()
+ss = rd.separateStat()
 
-x[0].plot(sharex=True, subplots=True, y=['KOUX', 'KOUY', 'KOUZ'])
+print (ss[5])
+
+ss[4].plot(sharex=True, subplots=True)
 plt.show()
+
+# inidate=datetime.datetime(2016,6,24,0,0), enddate=datetime.datetime(2016,6,28,0,0),
+#
+# station = 'kou'
+# year = '2015'
+# month = '05'
+# days = ['23']
+# host = 'ftp.intermagnet.org' # ftp host name
+# user = 'imaginpe' # ftp user name
+# password = 'd@a^DGE' # ftp password
+# # Directory contain the data
+# working_directory = 'minute/variation/IAGA2002/'+year+'/'+month+'/'
+#
+# dataDownlDir = '/home/jose/MEGA/Doutorado/Progs/read_plotSpec_Intermag/dados/data20180521125025/'
+#
+# listFilename = []
+# for d in days:
+#     listFilename.append(station+year+month+d+'vmin.min')
+#
+# print (listFilename)
+#
+# # filename = station+year+month+day+'vmin.min' # data filename
+#
+# # Creating a instance of MarxDownloader
+# mx = DataDownloader(host, user, password)
+#
+# mx.set_user_and_password(user, password)
+#
+#
+# # Connecting
+# mx.connectFTP()
+#
+# mx.set_output_directory(dataDownlDir)
+#
+# mx.set_directoryFTP(working_directory)
+#
+# mx.download_one_dataFTP(listFilename[0])
+#
+#
+#
+#
+# # rd = ReadCarisma(inidate=datetime.datetime(2014,03,25,00,00), enddate=datetime.datetime(2014,03,25,23,59))
+# rd = ReadIntermagnet(filen = listFilename[0])
+#
+# rd.directories(downlDir='/home/jose/MEGA/Doutorado/Progs/read_plotSpec_Intermag/dados/data20180521125025/', plotDir='/home/jose/MEGA/Doutorado/Progs/read_SuperMag/plots/')
+#
+# rd.files()
+#
+# x = rd.readInfo()
+#
+# # x = rd.readData()
+#
+# x[0].plot(sharex=True, subplots=True, y=['KOUX', 'KOUY', 'KOUZ'])
+# plt.show()
